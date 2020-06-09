21695f34a05b7351356befd51f6d80bade31d455
bob <golusherawat@gmail.com>
2020-06-09 21:14:51 +0530

    geetika design added

--- models/subPlace.js
+++ models/subPlace.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const subPlaceSchema = new Schema({
+const SubPlaceSchema = new Schema({
     title   :{type:String , trim:true , default:""},
     image   :{type:String , trim:true , default:""},
     content :{type:String , default:""}
@@ -9,4 +9,4 @@ const subPlaceSchema = new Schema({
 
 })
 
-module.exports = mongoose.model('SubPlace',subPlaceSchema);
+module.exports = mongoose.model('SubPlace',SubPlaceSchema);
